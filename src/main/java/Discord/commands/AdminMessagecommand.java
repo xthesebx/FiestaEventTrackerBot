c11854c75a6208569bb465da45b75c49c3d349c7
xXTheSebXx <sebastianjerke@gmail.com>
2024-04-04 22:06:59 +0200

    remove empty line

--- src/main/java/Discord/commands/AdminMessagecommand.java
+++ src/main/java/Discord/commands/AdminMessagecommand.java
@@ -6,7 +6,6 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 public class AdminMessagecommand extends AdminCommands {
 	public AdminMessagecommand (MessageReceivedEvent event, String s) throws NotAdminException {
 		super(event);
-		
 		Main.sendToAll(s.substring(s.indexOf(" ") + 1));
 	}
 }
